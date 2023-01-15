4c75877fbc3fe94495406a71a256f8621c1f8517
Nikoloz Naskidashvili <nikoloz2005@gmail.com>
2023-01-15 14:02:14 +0400

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,17 @@
+fn main() {
+    let welcome_message = r#"
+       ___           __       ___  _     ____                 __
+      / _ \__ _____ / /___ __/ _ \(_)___/ __/__ ___ _________/ /
+     / , _/ // (_-</ __/ // / // / / __/\ \/ -_) _ `/ __/ __/ _ \
+    /_/|_|\_,_/___/\__/\_, /____/_/_/ /___/\__/\_,_/_/  \__/_//_/
+                      /___/
+
+    _______________________
+    By NickNaskida
+
+    Github: https://github.com/NickNaskida
+    Project: https://github.com/NickNaskida/RustyDirSearch
+    "#;
+
+    println!("{}", welcome_message);
+}
